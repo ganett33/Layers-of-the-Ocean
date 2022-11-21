193111a090102ffdaf3c03431c74d3dbed9a703c
sid <ganett33@gmail.com>
2022-11-21 23:17:49 +1300

    amend the hero section animation & change font hero#text

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let sky = document.getElementById('sky');
-let islands = document.getElementById('islands');
+let cloud = document.getElementById('cloud');
+let islandsLeft = document.getElementById('islandsLeft');
+let islandsRight = document.getElementById('islandsRight');
 let sea = document.getElementById('sea');
 let text = document.getElementById('text');
 let btn = document.getElementById('btn');
@@ -7,10 +9,15 @@ let header = document.getElementById('header');
 
 window.addEventListener('scroll', function () {
   let value = window.scrollY;
-  sky.style.top = value * 0.1 + 'px';
-  islands.style.top = value * 0.3 + 'px';
-  sea.style.top = value * 0.12 + 'px';
+
+  sky.style.top = value * 0.4 + 'px';
+  cloud.style.left = value * -0.5 + 'px';
+  islandsLeft.style.top = value * 0.4 + 'px';
+  islandsRight.style.top = value * 0.4 + 'px';
+  sea.style.top = value * 0.35 + 'px';
+  header.style.left = value * 0.4 + 'px';
+
+  cloud.style.marginTop = value * 1 + 'px';
   text.style.marginTop = value * 0.1 + 'px';
   btn.style.marginTop = value * 0.5 + 'px';
-  header.style.left = value * 0.4 + 'px';
 });
